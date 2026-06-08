db084ec9f3d3d12e0149e6dae0d90f1cbac72182
lukeBishop133 <luke.bishop@rdgstudent.com>
2026-06-08 13:43:25 -0400

    Add conditional statement to hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -3,5 +3,13 @@
 int main() {
     printf("Hello, World!\n");
     printf("This is a dumb C program. \n");
+
+    int num = 5;
+    if(num < 6 || num < 7)
+    {
+        printf("I'm a chudling");
+    }
+    
     return 0;
+    
 }
