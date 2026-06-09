931c974b198d50c4eede80c9ef948ed411647744
lukeBishop133 <luke.bishop@rdgstudent.com>
2026-06-09 16:58:32 +0000

    added

--- hello_world.c
+++ hello_world.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    printf("This is a dumb C program. \n");
-
-    int num = 5;
-    if(num < 6 || num < 7)
-    {
-        printf("I'm a chudling");
+int main(void)
+{
+    int *data = malloc(20 * sizeof(int));
+    if (data == NULL) {
+        fprintf(stderr, "Failed to allocate memory\n");
+        return 1;
     }
-    
+
+    for (int i = 0; i < 20; i++) {
+        data[i] = i;
+    }
+    int value = 5;
+    printf("value = %d\n", value);
+
+    printf("array =");
+    for (int i = 0; i < 20; i++) {
+        printf(" %d", data[i]);
+    }
+    printf("\n");
+
+    free(data);
     return 0;
-    
 }
