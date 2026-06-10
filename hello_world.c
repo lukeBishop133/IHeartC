3b4281d39d3639a3c52cd606ad52a05cc51e7779
lukeBishop133 <luke.bishop@rdgstudent.com>
2026-06-10 14:59:15 +0000

    commited

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+int add(int a, int b);
+
 
 int main(void)
 {
+    srand(time(NULL));
     int *data = malloc(20 * sizeof(int));
     if (data == NULL) {
         fprintf(stderr, "Failed to allocate memory\n");
@@ -10,7 +15,7 @@ int main(void)
     }
 
     for (int i = 0; i < 20; i++) {
-        data[i] = i;
+        data[i] = 100 + (rand() % 101);
     }
     int value = 5;
     printf("value = %d\n", value);
@@ -19,8 +24,16 @@ int main(void)
     for (int i = 0; i < 20; i++) {
         printf(" %d", data[i]);
     }
+    
     printf("\n");
-
     free(data);
+
+    int sum = add(6 + 6);
+    printf("The sum of 5 and 10 is %d \n", sum);
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
